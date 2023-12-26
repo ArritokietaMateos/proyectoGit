e55240e52a78560d649afbde4cce66086867f361
Arritxu Mateos <iramateos@lhusurbil.eus>
2023-12-27 00:37:58 +0100

    commit Metodo1

--- src/App.java
+++ src/App.java
@@ -1,4 +1,8 @@
 public class App {
+
+    public void metodo1(){
+        System.out.println("metodo 1");
+    }
     public static void main(String[] args) throws Exception {
        
         System.out.println("Hello Repositorio Remoto Prueba!");

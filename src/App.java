a5cc3ce65989c9e027e52162ba662698ecf6d086
Arritxu Mateos <iramateos@lhusurbil.eus>
2023-12-28 23:06:25 +0100

    commit añadoComentario

--- src/App.java
+++ src/App.java
@@ -10,6 +10,12 @@ public class App {
         System.out.println("Hola Arritxu1 subo este mensaje desde cuenta Arritxu2");        
         System.out.println("Agur Arritxu2!");
 
+        //He hecho un pull me bajo todo. Y me pongo a programar y mientras otra persona ha hecho un push
+        //y ha subido un cambio. Entonces yo hago un push y me da error porque no tengo el cambio que ha subido
+        //la otra persona. Entonces hago un pull y me bajo el cambio que ha subido la otra persona. Y ahora ya
+        //puedo hacer un push y subir mi cambio.
+
+        System.out.println("Cambio  en el fichero App.java desde cuenta Arritxu1");
 
     }
 }

1f588a95a29674cdc84f5759ae92fa11be6d51f1
arritxu <arritxums@gmail.com>
2024-01-30 12:35:03 +0100

    commit unCommit

--- src/App.java
+++ src/App.java
@@ -7,6 +7,7 @@ public class App {
        
         System.out.println("Borro todos los mensajes");
         System.out.println("Cambio  en el fichero App.java desde cuenta Arritxu1");
+        System.out.println("Cambio  en el fichero App.java desde cuenta Arritxu2");
 
     }
 }
